40d3ec067b3baba10ec06e148d38054f532a597b
Haunter999 <lpopok@gmail.com>
2011-07-29 14:01:59 +0400

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,16 +3,21 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+   @base_title="Ruby on Rails Tutorial Sample App"
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-it "should have the right title" do
-  get 'home'
-  response.should have_selector("title",
-                    :content => "Ruby on Rails Tutorial Sample App | Home")
-end
+
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Home")
+    end
   end
 
   describe "GET 'contact'" do
@@ -20,11 +25,12 @@ end
       get 'contact'
       response.should be_success
     end
-it "should have the right title" do
-  get 'contact'
-  response.should have_selector("title",
-                    :content => "Ruby on Rails Tutorial Sample App | Contact")
-end
+
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Contact")
+    end
   end
 
   describe "GET 'about'" do
@@ -32,10 +38,25 @@ end
       get 'about'
       response.should be_success
     end
+
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title",
+                                    :content => @base_title + " | About")
+    end
+  end
+
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
 it "should have the right title" do
-  get 'about'
+  get 'help'
   response.should have_selector("title",
-                    :content => "Ruby on Rails Tutorial Sample App | About")
+                    :content => @base_title + " | Help")
 end
   end
-end
+  
+  end
+
